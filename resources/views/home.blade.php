47b85343bb255d6d9648d1af720d2d2aa10fafb2
jsoliz064 <jsoliz064@gmail.com>
2021-10-02 01:16:55 -0400

    inicio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,84 @@
 @extends('adminlte::page')
 
-@section('title', 'Dashboard')
+@section('title', 'Inicio')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+<head>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@1,900&display=swap" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@500&display=swap" rel="stylesheet">
+
+</head>
+<body>
+	<!--cliente, proveedor, productos¿,personal-->
+    <div class="container">
+        <div class="cabecera"style="display: flex">
+            <img src="{{asset('imagenes/logo.png')}}" width="100" height="100">
+            <h1 class= "title"> PACKS FICCT </h1>
+        </div>
+    </div>
+
+	<div class= "menu" >
+        <div class="card-deck dashboard-card-deck">
+            <div class="card dashboard-card" role="listitem" data-region="course-contend">
+                <a href="">
+                    <div class="card.img dashboard-card-img" style="color:#30ceae">
+                        <img src="{{asset('imagenes/logo.png')}}" width="150" height="150">
+                    </div>
+                    <div class="card-body pr-1 course-info-container" >
+                        <h4>1er Semestre</h4>
+                    </div>
+                </a>
+            </div>
+
+            <div class="card dashboard-card" role="listitem" data-region="course-contend">
+                <a href="">
+                    <div class="card.img dashboard-card-img" style="color:#30ceae">
+                        <img src="{{asset('imagenes/logo.png')}}" width="150" height="150">
+                    </div>
+                    <div class="card-body pr-1 course-info-container" >
+                        <h4>2do Semestre</h4>
+                    </div>
+                </a>
+            </div>
+
+            <div class="card dashboard-card" role="listitem" data-region="course-contend">
+                <a href="">
+                    <div class="card.img dashboard-card-img" style="color:#30ceae">
+                        <img src="{{asset('imagenes/logo.png')}}" width="150" height="150">
+                    </div>
+                    <div class="card-body pr-1 course-info-container" >
+                        <h4>3er Semestre</h4>
+                    </div>
+                </a>
+            </div>
+
+            <div class="card dashboard-card" role="listitem" data-region="course-contend">
+                <div class="card.img dashboard-card-img" style="color:#30ceae">
+                    <img src="{{asset('imagenes/logo.png')}}" width="150" height="150">
+                </div>
+                <div class="card-body pr-1 course-info-container" >
+                    <h4>5to Semestre</h4>
+                </div>
+            </div>
+        </div>
+	
+</body>
+
+
 @stop
 
 @section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
+    <link rel="stylesheet" href="css/home.css">
+	
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
 @stop
