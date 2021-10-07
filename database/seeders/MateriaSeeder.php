b10002544f622932d9406ffe69bed7c90fb65934
darwin mamani <darwinjr40@gmail.com>
2021-10-07 10:46:23 -0400

    darwin

--- database/seeders/MateriaSeeder.php
+++ database/seeders/MateriaSeeder.php
@@ -14,6 +14,7 @@ class MateriaSeeder extends Seeder
      */
     public function run()
     {
+        //semestre 1    -----------------------------------------------------------
         Materia::create([
             'id_semestre'=> '1',
             'nombre'=>'Introduccion a la informatica',
@@ -36,9 +37,89 @@ class MateriaSeeder extends Seeder
         ]);
 
         Materia::create([
+            'id_semestre'=> '1',
+            'nombre'=>'CALCULO I',
+            'sigla'=> 'MAT101'
+        ]);
+
+        //semestre 2-----------------------------------------------------------
+        Materia::create([
             'id_semestre'=> '2',
-            'nombre'=>'Fisica 2',
+            'nombre'=>'FISICA II',
             'sigla'=> 'FIS101'
+        ]);
+
+        
+        Materia::create([
+            'id_semestre'=> '2',
+            'nombre'=>'',
+            'sigla'=> ''
+        ]);
+
+        Materia::create([
+            'id_semestre'=> '2',
+            'nombre'=>'PROGRAMACION I',
+            'sigla'=> 'INF120'
+        ]);
+
+        Materia::create([
+            'id_semestre'=> '2',
+            'nombre'=>'INGLES TECNICO II',
+            'sigla'=> 'LIN101'
+        ]);
+
+        Materia::create([
+            'id_semestre'=> '2',
+            'nombre'=>'CALCULO II',
+            'sigla'=> 'MAT102'
+        ]);
+
+        Materia::create([
+            'id_semestre'=> '2',
+            'nombre'=>'ALGEBRA LINEAL',
+            'sigla'=> 'MAT103'
+        ]);
+        //semestre 3-----------------------------------------------------------
+        Materia::create([
+            'id_semestre'=> '3',
+            'nombre'=>'ADMINISTRACION',
+            'sigla'=> 'ADM100'
+        ]);
+
+        Materia::create([
+            'id_semestre'=> '3',
+            'nombre'=>'TEORIA DE CAMPOS',
+            'sigla'=> 'ELT241'
+        ]);
+
+        Materia::create([
+            'id_semestre'=> '3',
+            'nombre'=>'FISICA III',
+            'sigla'=> 'FIS200'
+        ]);
+
+        Materia::create([
+            'id_semestre'=> '3',
+            'nombre'=>'PROGRAMACION II',
+            'sigla'=> 'INF210'
+        ]);
+
+        Materia::create([
+            'id_semestre'=> '3',
+            'nombre'=>'ARQUITECTURA DE COMPUTADORAS',
+            'sigla'=> 'INF211'
+        ]);
+
+        Materia::create([
+            'id_semestre'=> '3',
+            'nombre'=>'ECUACIONES DIFERENCIALES',
+            'sigla'=> 'MAT207'
+        ]);
+
+        Materia::create([
+            'id_semestre'=> '3',
+            'nombre'=>'ANALISIS DE CIRCUITOS',
+            'sigla'=> 'RDS210'
         ]);
          //semestre 4-----------------------------------------------------------
          Materia::create([
@@ -173,65 +254,252 @@ class MateriaSeeder extends Seeder
             'sigla'=> 'RDS320'
         ]);
 
+        //semestre 7-----------------------------------------------------------
+            
+
+            Materia::create([
+                'id_semestre'=> '7',
+                'nombre'=>'SISTEMAS DE INFORMACION II',
+                'sigla'=> 'INF412'
+            ]);
+
+            Materia::create([
+                'id_semestre'=> '7',
+                'nombre'=>'SISTEMAS OPERATIVOS II',
+                'sigla'=> 'INF413'
+            ]);
+
+            Materia::create([
+                'id_semestre'=> '7',
+                'nombre'=>'INTELIGENCIA ARTIFICIAL',
+                'sigla'=> 'INF418'
+            ]);
+
+            Materia::create([
+                'id_semestre'=> '7',
+                'nombre'=>'SISTEMAS PARA EL SOPORTE A LA TOMA',
+                'sigla'=> 'INF432'
+            ]);
+
+            Materia::create([
+                'id_semestre'=> '7',
+                'nombre'=>'REDES I',
+                'sigla'=> 'INF433'
+            ]);
+
+            Materia::create([
+                'id_semestre'=> '7',
+                'nombre'=>'INVESTIGAC.OPERATIVA II',
+                'sigla'=> 'MAT419'
+            ]);
+
+            Materia::create([
+                'id_semestre'=> '7',
+                'nombre'=>'APLICACIONES CON MICROPROCESAD.',
+                'sigla'=> 'RDS410'
+            ]);
+        //semestre 8-----------------------------------------------------------
+            Materia::create([
+                'id_semestre'=> '8',
+                'nombre'=>'PREPARAC.Y EVALUAC.DE PROYECTOS',
+                'sigla'=> 'ECO449'
+            ]);
+
+            Materia::create([
+                'id_semestre'=> '8',
+                'nombre'=>'SISTEMAS DE COMUNICACION II',
+                'sigla'=> 'ELT384'
+            ]);
+
+            Materia::create([
+                'id_semestre'=> '8',
+                'nombre'=>'INGENIERIA DE SOFTWARE I',
+                'sigla'=> 'INF422'
+            ]);
+
+            Materia::create([
+                'id_semestre'=> '8',
+                'nombre'=>'REDES II',
+                'sigla'=> 'INF423'
+            ]);
+
+            Materia::create([
+                'id_semestre'=> '8',
+                'nombre'=>'SISTEMAS EXPERTOS',
+                'sigla'=> 'INF428'
+            ]);
+
+            Materia::create([
+                'id_semestre'=> '8',
+                'nombre'=>'SISTEMAS DE INFORM.GEOGRAFICA',
+                'sigla'=> 'INF442'
+            ]);
+
+            Materia::create([
+                'id_semestre'=> '8',
+                'nombre'=>'AUDITORIA INFORMATICA',
+                'sigla'=> 'INF462'
+            ]);
+
+            Materia::create([
+                'id_semestre'=> '8',
+                'nombre'=>'TALLER DE ANALISIS Y DISENO DE REDE',
+                'sigla'=> 'RDS421'
+            ]);
+
+            Materia::create([
+                'id_semestre'=> '8',
+                'nombre'=>'LEGISLAC.EN REDES Y COMUNICACIONES',
+                'sigla'=> 'RDS429'
+            ]);
+
+        //semestre 9-----------------------------------------------------------
+        Materia::create([
+            'id_semestre'=> '9',
+            'nombre'=>'TALLER DE GRADO I',
+            'sigla'=> 'INF511'
+        ]);
+
+        Materia::create([
+            'id_semestre'=> '9',
+            'nombre'=>'INGENIERIA DE SOFTWARE II',
+            'sigla'=> 'INF512'
+        ]);
+
+        Materia::create([
+            'id_semestre'=> '9',
+            'nombre'=>'TECNOLOGIA WEB',
+            'sigla'=> 'INF513'
+        ]);
+
+        Materia::create([
+            'id_semestre'=> '9',
+            'nombre'=>'ARQUITECTURA DEL SOFTWARE',
+            'sigla'=> 'INF552'
+        ]);
+
+        Materia::create([
+            'id_semestre'=> '9',
+            'nombre'=>'GESTION Y ADMINISTRACION DE REDES',
+            'sigla'=> 'RDS511'
+        ]);
+
+        Materia::create([
+            'id_semestre'=> '9',
+            'nombre'=>'REDES INALAMBRIC.Y COMUNIC.MOVILES',
+            'sigla'=> 'RDS512'
+        ]);
+
+        Materia::create([
+            'id_semestre'=> '9',
+            'nombre'=>'SEGURIDAD EN REDES Y TRANSMIS.DE DA',
+            'sigla'=> 'RDS519'
+        ]);
+
+
         //Electivas---------------------------------------------------------------
         
         //5to semestre
         Materia::create([
-            'id_semestre'=> '5',
+            'id_semestre'=> '10',
             'nombre'=>'COSTOS Y PRESUPUESTOS',
             'sigla'=> 'ELC002'
         ]);
 
         Materia::create([
-            'id_semestre'=> '5',
+            'id_semestre'=> '10',
             'nombre'=>'PROGRAMACION GRAFICA',
             'sigla'=> 'ELC102'
         ]);
 
         Materia::create([
-            'id_semestre'=> '5',
+            'id_semestre'=> '10',
             'nombre'=>'DISENO DE CIRCUITOS INTEGRADOS',
             'sigla'=> 'ELC201'
         ]);
 
         Materia::create([
-            'id_semestre'=> '5',
+            'id_semestre'=> '10',
             'nombre'=>'SISTEMAS LOGICOS Y DIGITALES I',
             'sigla'=> 'ELT352'
         ]);
 
         Materia::create([
-            'id_semestre'=> '5',
+            'id_semestre'=> '10',
             'nombre'=>'SENALES Y SISTEMAS',
             'sigla'=> 'ELT354'
         ]);
 
         //6tosemestre
         Materia::create([
-            'id_semestre'=> '6',
+            'id_semestre'=> '10',
             'nombre'=>'PRODUCCION Y MARKETING',
             'sigla'=> 'ELC003'
         ]);
 
         Materia::create([
-            'id_semestre'=> '6',
+            'id_semestre'=> '10',
             'nombre'=>'TOPIC.AVANZ.DE PROGRAMAC.',
             'sigla'=> 'ELC103'
         ]);
 
         Materia::create([
-            'id_semestre'=> '6',
+            'id_semestre'=> '10',
             'nombre'=>'TELEVISION DIGITAL',
             'sigla'=> 'ELC204'
         ]);
 
         Materia::create([
-            'id_semestre'=> '6',
+            'id_semestre'=> '10',
             'nombre'=>'SISTEMAS LOGICOS Y DIGITALES II',
             'sigla'=> 'ELT362'
         ]);
 
+        //7mo smeestre
 
+        Materia::create([
+            'id_semestre'=> '10',
+            'nombre'=>'INGENIERIA DE LA CALIDAD',
+            'sigla'=> 'ELC005'
+        ]);
+
+        Materia::create([
+            'id_semestre'=> '10',
+            'nombre'=>'SISTEMAS DISTRIBUIDOS',
+            'sigla'=> 'ELC105'
+        ]);
+
+        Materia::create([
+            'id_semestre'=> '10',
+            'nombre'=>'LINEAS DE TRANSMIS.Y ANTENAS',
+            'sigla'=> 'ELC206'
+        ]);
+
+        Materia::create([
+            'id_semestre'=> '10',
+            'nombre'=>'SISTEMAS DE COMUNICACION I',
+            'sigla'=> 'ELT374'
+        ]);
+
+        //8vo semestre
+
+        Materia::create([
+            'id_semestre'=> '10',
+            'nombre'=>'LEGISL.EN CIENCIAS DE LA COMPUTAC.',
+            'sigla'=> 'ELC008'
+        ]);
+
+        Materia::create([
+            'id_semestre'=> '10',
+            'nombre'=>'CRIPTOGRAFIA Y SEGURIDAD',
+            'sigla'=> 'ELC107'
+        ]);
+
+        Materia::create([
+            'id_semestre'=> '10',
+            'nombre'=>'REDES ADHOC',
+            'sigla'=> 'ELC208'
+        ]);
 
 
     }
