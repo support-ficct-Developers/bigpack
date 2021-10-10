03e017632bbd8f1714f6fa3a1e9d3c088a6ab252
Fernando Cayola Cardona <fernandocayolacar+gmail@gmail.com>
2021-10-10 15:38:41 -0400

    Un peji se olvido el footer

--- resources/views/developer/dev.blade.php
+++ resources/views/developer/dev.blade.php
@@ -164,7 +164,45 @@
          </section>
          </main>
 
-
+<footer class="footer">
+        <div class="footer_bg">
+            <div class="footer__content">
+            <div class="footer_container container grid">
+                <div>
+                    <h1 class="footer_title">Support <!-- <img src="imagenes/logo.png" alt="" class="footer__img"> --></h1>
+                    <h1 class="footer_title">Aprende + <!-- <img src="imagenes/logo.png" alt="" class="footer__img"> --></h1>
+                    <span class="footer_subtitle">#Estamos_para_ayudarte <br> #Cuenta_con_nosotros</span>            </div>
+                <ul class="footer_links">
+                    <li>
+                        <a href="#" class="footer_link">Inicio</a>
+                    </li>
+                    <li>
+                        <a href="http://supportficct.com:81/postSemestres#semestres" class="footer_link">Semestres</a>
+                    </li>
+                    <li>
+                        <a href="http://supportficct.com:81/developers" class="footer_link">Desarrolladores</a>
+                    </li>
+                </ul>
+                <div class="footer_socials">
+                    <a href="https://www.facebook.com/supportUAGRM" target="_blank" class="footer_social">
+                        <i class="uil uil-facebook-f"></i>
+                    </a>
+                    <a href="https://www.youtube.com/channel/UCz9Q7IBBcKw78nqP7V54WBA" target="_blank" class="footer_social">
+                        <i class="uil uil-youtube"></i>
+                    </a>
+                    <a href="https://chat.whatsapp.com/IIZZdKyTiW03F9IOdDpTKZ" target="_blank" class="footer_social">
+                        <i class="uil uil-whatsapp"></i>
+                    </a>
+                    <a href="https://t.me/joinchat/Cohi-d79IsViMGIx" target="_blank" class="footer_social">
+                        <i class="uil uil-telegram-alt"></i>
+                    </a>
+                    
+                </div>
+            </div>
+        </div>
+            <p class="footer_copy">© SUPPORT. Todos los derechos Reservados</p>
+        </div>
+    </footer>
         
             <a href="#" class="scrollup" id="scroll-up">
                 <i class="uil uil-arrow-up scrollup_icon"></i>
