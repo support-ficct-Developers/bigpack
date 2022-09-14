853d2438d08ca1244bb8a50e0cd531746417fc70
jsoliz064 <jsoliz064@gmail.com>
2022-09-14 17:50:42 -0400

    view Preloader

---
+++ resources/views/vendor/adminlte/partials/common/preloader.blade.php
@@ -0,0 +1,12 @@
+<div class="preloader flex-column justify-content-center align-items-center">
+
+    {{-- Preloader logo --}}
+    {{--  <img src="{{ asset(config('adminlte.preloader.img.path', 'imagenes/intercentro.png')) }}"
+         class="{{ config('adminlte.preloader.img.effect', 'animation__shake') }}"
+         alt="{{ config('adminlte.preloader.img.alt', 'AdminLTE Preloader Image') }}"
+         width="{{ config('adminlte.preloader.img.width', 60) }}"
+         height="{{ config('adminlte.preloader.img.height', 60) }}">  --}}
+         <img src="{{asset('imagenes/intercentro.png')}}" class="{{ config('adminlte.preloader.img.effect', 'animation__shake') }}" alt="Preloader Logo intercentro" width="200px"
+         height="200px">
+
+</div>
