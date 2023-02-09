b4acbce9a02b48f7a7234c46dc7e97121485ce31
jsoliz064 <jsoliz064@gmail.com>
2023-02-09 15:20:40 -0400

    developers

--- resources/views/developer/dev.blade.php
+++ resources/views/developer/dev.blade.php
@@ -20,40 +20,7 @@
                  {{--  <image class="" width="8%" height="8%" src="{{asset('imagenes/logoo.png')}}"/>  --}}
                 </h2>
              <span class="section_subtitle">Acerca de nosotros</span> 
-             <div class="about_container container grid">
-                 <div class="home_img">
-                     <svg class="home_blob"viewBox="0 0 200 187" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                         <mask id="mask0" mask-type="alpha">
-                             <path d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 165.547 
-                             130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 129.362C2.45775 
-                             97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 -0.149132 97.9666 
-                             0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z"/>
-                         </mask>
-                         <g mask="url(#mask0)">
-                             <path d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 
-                             165.547 130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 
-                             129.362C2.45775 97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 
-                             -0.149132 97.9666 0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z"/>
-         
-                             <image class="home_blob-img" x='12' y='18' xlink:href="{{asset('imagenes/cayola.png')}}"/>
-                         </g>
-                     </svg>                 
-         
-                 </div>
-                 <!-- <img src="./img/ESTO.png" alt="" class="about_img"> -->
-                 <div class="about_data">
-                     <h1 class="desa_title">Fernando</h1>
-                     <span class="desa_subtitle">Frontend-Developer</span>
-                     <p class="about_description">Estudiante de la Carrera de Ingenieria en Redes y Telecomunicaciones 
-                         de la Universidad Autónoma Gabriel René Moreno</p>
-                     
-                     <div class="about_buttons">
-                         <a href="http://wa.me//+59173665042" class="button button--flex">
-                             Contáctame<i class="uil uil-message button_icon"></i>
-                         </a>
-                     </div>
-                 </div>
-             </div>
+             
          
              <div class="about_container container grid">
                  <div class="home_img">
@@ -89,6 +56,41 @@
                      </div>
                  </div>
              </div>
+
+             <div class="about_container container grid">
+                <div class="home_img">
+                    <svg class="home_blob"viewBox="0 0 200 187" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                        <mask id="mask0" mask-type="alpha">
+                            <path d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 165.547 
+                            130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 129.362C2.45775 
+                            97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 -0.149132 97.9666 
+                            0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z"/>
+                        </mask>
+                        <g mask="url(#mask0)">
+                            <path d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 
+                            165.547 130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 
+                            129.362C2.45775 97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 
+                            -0.149132 97.9666 0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z"/>
+        
+                            <image class="home_blob-img" x='12' y='18' xlink:href="{{asset('imagenes/cayola.png')}}"/>
+                        </g>
+                    </svg>                 
+        
+                </div>
+                <!-- <img src="./img/ESTO.png" alt="" class="about_img"> -->
+                <div class="about_data">
+                    <h1 class="desa_title">Fernando</h1>
+                    <span class="desa_subtitle">Frontend-Developer</span>
+                    <p class="about_description">Estudiante de la Carrera de Ingenieria en Redes y Telecomunicaciones 
+                        de la Universidad Autónoma Gabriel René Moreno</p>
+                    
+                    <div class="about_buttons">
+                        <a href="http://wa.me//+59173665042" class="button button--flex">
+                            Contáctame<i class="uil uil-message button_icon"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
           
              <div class="about_container container grid">
                  <div class="home_img">
